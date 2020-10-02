f25e50ba112a374e29a7030b796b6e2beaa76c74
GuyBR <gbrobbe@gmail.com>
2020-10-01 22:13:10 -0300

    FIX: Icon & Details finance section

--- src/components/EventFinanceSection/index.tsx
+++ src/components/EventFinanceSection/index.tsx
@@ -11,7 +11,6 @@ import {
   SupplierButton,
   TransactionsWindow,
   MenuButton,
-  SupplierTransactionsWindow,
   SupplierTransactionAgreementsWindow,
 } from './styles';
 import ITransactionAgreementDTO from '../../dtos/ITransactionAgreementDTO';
