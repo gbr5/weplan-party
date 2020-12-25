4ff3876b87536f4ffea7ccf84d2630e2be7c0362
GuyBR <gbrobbe@gmail.com>
2020-12-25 18:23:52 -0300

    S_73-Styles

--- src/components/GuestToUserMessageWindow/index.tsx
+++ src/components/GuestToUserMessageWindow/index.tsx
@@ -1,12 +1,14 @@
-import React, { memo, MouseEventHandler, useCallback } from 'react';
+import React, { memo, MouseEventHandler, useCallback, useMemo } from 'react';
 import { FiCheckSquare, FiSquare } from 'react-icons/fi';
 
 import IEventGuestDTO from '../../dtos/IEventGuestDTO';
 import { useToast } from '../../hooks/toast';
 import api from '../../services/api';
 import WindowUnFormattedContainer from '../WindowUnFormattedContainer';
+import placeholder from '../../assets/WePlanLogo.svg';
 
 import { Container, SideMenu, Body, Message } from './styles';
+import formatStringToDate from '../../utils/formatDateToString';
 
 interface IPropsDTO {
   eventGuest: IEventGuestDTO;
@@ -44,6 +46,16 @@ const GuestToUserMessageWindow: React.FC<IPropsDTO> = ({
     }
   }, [addToast, getEventsAsGuest, handleCloseWindow, eventGuest]);
 
+  const avatar = useMemo(() => {
+    const xAvatar =
+      eventGuest.weplanGuest &&
+      eventGuest.weplanGuest.weplanUserGuest &&
+      eventGuest.weplanGuest.weplanUserGuest.avatar_url
+        ? eventGuest.weplanGuest.weplanUserGuest.avatar_url
+        : placeholder;
+    return xAvatar;
+  }, [eventGuest]);
+
   return (
     <WindowUnFormattedContainer
       onHandleCloseWindow={onHandleCloseWindow}
@@ -57,30 +69,71 @@ const GuestToUserMessageWindow: React.FC<IPropsDTO> = ({
     >
       <Container>
         <SideMenu>
-          <h1>@{eventGuest.host.name}</h1>
+          <img src={avatar} alt="WePlan User Avatar" />
+          <h1>@{eventGuest.host.trimmed_name}</h1>
           <h2>
-            {eventGuest.host.personInfo.first_name}{' '}
+            <strong>Anfitrião:</strong> {eventGuest.host.personInfo.first_name}{' '}
             {eventGuest.host.personInfo.last_name}
           </h2>
-          <button type="button" onClick={handleEditConfirmedGuest}>
-            {eventGuest.confirmed ? <FiCheckSquare /> : <FiSquare />}
-          </button>
+          <h2>
+            <strong>Evento:</strong> {eventGuest.weplanGuest.event.name}
+          </h2>
+          <h2>
+            <strong>Data:</strong>{' '}
+            {formatStringToDate(String(eventGuest.weplanGuest.event.date))}
+          </h2>
+          <h2>
+            <strong>Traje:</strong>{' '}
+            {eventGuest.weplanGuest.event.eventInfo &&
+              eventGuest.weplanGuest.event.eventInfo.dress_code &&
+              eventGuest.weplanGuest.event.eventInfo.dress_code}
+          </h2>
+          <h2>
+            <strong>RSVP:</strong>
+            <button type="button" onClick={handleEditConfirmedGuest}>
+              {eventGuest.confirmed ? (
+                <>
+                  <p>Confirmado</p>
+                  <FiCheckSquare />
+                </>
+              ) : (
+                <>
+                  <p>Não confirmado</p>
+                  <FiSquare />
+                </>
+              )}
+            </button>
+          </h2>
         </SideMenu>
         <Body>
           {eventGuest.weplanGuest.userConfirmations.map(conversation => {
             return (
-              <Message>
-                <h3>{conversation.title}</h3>
-                <p>{conversation.message}</p>
-                <div>
-                  {conversation.userConfirmationFiles.map(file => {
-                    return (
-                      <a target="blank" href={file.file.file_url}>
-                        {file.file.file_name}
-                      </a>
-                    );
-                  })}
-                </div>
+              <Message
+                isUser={conversation.sender_id === eventGuest.weplanGuest.id}
+              >
+                {conversation.sender_id !== eventGuest.weplanGuest.id && (
+                  <h1>@{eventGuest.host.trimmed_name}</h1>
+                )}
+                <h3>
+                  <strong>Título: </strong>
+                  {conversation.title}
+                </h3>
+                <p>
+                  <strong>Mensagem: </strong>
+                  {conversation.message}
+                </p>
+                {eventGuest.weplanGuest.userConfirmations.length > 0 && (
+                  <div>
+                    <h3>Arquivos</h3>
+                    {conversation.userConfirmationFiles.map(file => {
+                      return (
+                        <a target="blank" href={file.file.file_url}>
+                          {file.file.file_name}
+                        </a>
+                      );
+                    })}
+                  </div>
+                )}
               </Message>
             );
           })}
--- src/components/GuestToUserMessageWindow/styles.ts
+++ src/components/GuestToUserMessageWindow/styles.ts
@@ -1,34 +1,78 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 26px;
+  height: 100%;
 
   display: grid;
-  grid-template-columns: 1fr 5fr;
+  grid-template-columns: 1fr 4fr;
   justify-content: flex-end;
   align-items: center;
-  gap: 8px;
+  gap: 32px;
+
+  box-sizing: border-box;
 `;
 
 export const SideMenu = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   gap: 16px;
   margin-right: auto;
+  align-items: stretch;
+  justify-content: stretch;
+
+  img {
+    height: 160px;
+    width: 100%;
+    border-radius: 8px;
+    box-shadow: var(--box-shadow);
+  }
 
   > h2 {
+    display: flex;
+    strong {
+      margin-right: 16px;
+      border-bottom: 1px solid var(--primary-color);
+      font-weight: 500;
+      color: var(--letter-color-4);
+    }
     text-align: left;
-    width: 200px;
+    width: 100%;
     font-size: 16px;
-    color: var(--letter-color-4);
+    color: var(--primary-color);
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5rem;
+    background: transparent;
+    border: none;
+    border-radius: 8px;
+    transition: 0.3s;
+    box-shadow: var(--box-shadow);
+
+    &:hover {
+      box-shadow: var(--box-shadow-hover);
+      background-color: rgba(255, 125, 25, 0.2);
+    }
   }
 `;
 
 export const Body = styled.div`
-  display: flex;
+  width: 100%;
+  height: 100%;
+  display: block;
   gap: 16px;
   margin-right: auto;
+  background: var(--letter-color-2);
+  padding: 16px;
+  align-items: flex-start;
+  justify-content: start;
+  border-radius: 8px;
 
   > h2 {
     text-align: left;
@@ -38,16 +82,72 @@ export const Body = styled.div`
   }
 `;
 
-export const Message = styled.div`
+interface IMessageProps {
+  isUser: boolean;
+}
+
+export const Message = styled.div<IMessageProps>`
+  margin: auto 40px auto 0;
   display: flex;
   flex-direction: column;
-  gap: 16px;
+  gap: 4px;
   margin-right: auto;
+  background: rgba(255, 225, 200, 0.9);
+  width: 100%;
+  border-radius: 8px;
 
-  > h2 {
+  padding: 8px;
+
+  > h1 {
     text-align: left;
-    width: 200px;
-    font-size: 16px;
-    color: var(--letter-color-4);
+    width: 100%;
+    font-size: 14px;
+    font-weight: 500;
+    color: rgba(0, 0, 0, 0.8);
   }
+  > h3 {
+    text-align: left;
+    width: 100%;
+    font-size: 14px;
+    font-weight: 500;
+    color: var(--letter-color-5);
+
+    strong {
+      margin-right: 16px;
+      width: 150px;
+      border-bottom: 1px solid var(--secondary-color);
+      font-weight: 700;
+    }
+  }
+  > p {
+    text-align: left;
+    width: 100%;
+    font-size: 12px;
+    color: var(--letter-color-5);
+    strong {
+      margin-right: 16px;
+      width: 150px;
+      font-size: 14px;
+      border-bottom: 1px solid var(--secondary-color);
+      font-weight: 600;
+    }
+  }
+
+  div {
+    > h3 {
+      text-align: left;
+      font-size: 14px;
+      color: var(--letter-color-5);
+
+      border-bottom: 1px solid var(--title-color);
+      font-weight: 700;
+    }
+  }
+
+  ${props =>
+    props.isUser &&
+    css`
+      margin: auto 0 auto 40px;
+      background: rgba(255, 95, 50, 0.9);
+    `}
 `;
