866e74f5f587e0b478ad3e329b7a20cb549e8c6c
GuyBR <gbrobbe@gmail.com>
2021-01-03 17:02:53 -0300

    S_87 - Create Event Styles

--- src/components/CreateEventWindow/index.tsx
+++ src/components/CreateEventWindow/index.tsx
@@ -1,7 +1,6 @@
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
-import { MdClose } from 'react-icons/md';
 import { useToast } from '../../hooks/toast';
 import api from '../../services/api';
 import Input from '../Input';
@@ -76,7 +75,7 @@ const CreateEventWindow: React.FC<IProps> = ({
     handleGetMyEvents,
     selectedDate,
   ]);
-  const inputHeight = { height: '40px' };
+  const inputHeight = { height: '40px', width: '100%' };
 
   return (
     <WindowUnFormattedContainer
@@ -89,49 +88,40 @@ const CreateEventWindow: React.FC<IProps> = ({
         width: '100%',
       }}
     >
-      <Container>
-        <Form ref={formRef} onSubmit={handleCreateEvent}>
+      <Form ref={formRef} onSubmit={handleCreateEvent}>
+        <Container>
           <h1>Crie seu evento</h1>
 
-          <div>
-            <span>
-              <button type="button" onClick={onHandleCloseWindow}>
-                <MdClose size={30} />
-              </button>
-            </span>
+          <button type="button" onClick={() => handleEventTypeDrawer()}>
+            {tipoDeEvento || 'Selecionar tipo de Evento'}
+          </button>
+          <Input
+            containerStyle={inputHeight}
+            type="text"
+            placeholder="Nome do evento"
+            name="name"
+            onChange={e => setEventName(e.target.value)}
+          />
 
-            <button type="button" onClick={() => handleEventTypeDrawer()}>
-              {tipoDeEvento || 'Selecionar tipo de Evento'}
-            </button>
+          <Input
+            containerStyle={inputHeight}
+            type="time"
+            name="event_time"
+            onChange={e => setEventStartTime(e.target.value)}
+          />
 
-            <Input
-              containerStyle={inputHeight}
-              type="text"
-              placeholder="Nome do evento"
-              name="name"
-              onChange={e => setEventName(e.target.value)}
-            />
-
-            <Input
-              containerStyle={inputHeight}
-              type="time"
-              name="event_time"
-              onChange={e => setEventStartTime(e.target.value)}
-            />
-
-            <Input
-              containerStyle={inputHeight}
-              type="date"
-              name="date"
-              onChange={e => setSelectedDate(e.target.value)}
-            />
-          </div>
+          <Input
+            containerStyle={inputHeight}
+            type="date"
+            name="date"
+            onChange={e => setSelectedDate(e.target.value)}
+          />
 
           <button type="submit">
             <h3>Criar</h3>
           </button>
-        </Form>
-      </Container>
+        </Container>
+      </Form>
     </WindowUnFormattedContainer>
   );
 };
--- src/components/CreateEventWindow/styles.ts
+++ src/components/CreateEventWindow/styles.ts
@@ -9,96 +9,37 @@ export const Container = styled.div`
   background: var(--header-background-color);
   border-radius: 8px;
   gap: 4vh;
-  box-shadow: 2px 2px 15px 8px rgba(255, 150, 10, 0.3);
-  padding: 32px;
+  padding: 4vh;
+  width: 100%;
+  height: 100%;
 
   > h1 {
     font-size: 24px;
     color: var(--title-color);
-  }
-
-  > div {
-    margin: auto 0;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    gap: 16px;
-
-    > h1 {
-      font-size: 24px;
-      color: var(--title-color);
-      margin-bottom: 40px;
-    }
-
-    > button {
-      height: 40px;
-      width: 100%;
-      border-radius: 4px;
-      padding: 0 16px;
-      border: none;
-      margin: 16px;
-      color: var(--primary-color);
-      background: var(--secondary-color);
-      box-shadow: var(--window-box-shadow);
-
-      &:hover {
-        background: var(--primary-color);
-        color: var(--secondary-color);
-        box-shadow: var(--box-shadow);
-      }
-    }
-
-    span {
-      position: absolute;
-      top: 4px;
-      right: 4px;
-      > button {
-        background: transparent;
-        border: none;
-
-        svg {
-          color: red;
-        }
-      }
-    }
-    /*
-    > input {
-      height: 40px;
-      width: 90%;
-      border-radius: 4px;
-      padding: 0 16px;
-      border: none;
-      margin: 16px;
-    } */
-
-    > p {
-      margin-top: 32px;
-      font-size: 20px;
-      color: var(--primary-color);
-    }
-
-    div {
-      display: flex;
-      gap: 16px;
-      margin-right: 16px;
-
-      > input {
-        height: 40px;
-        width: 90%;
-        border-radius: 4px;
-        padding: 0 16px;
-        border: none;
-        margin: 16px;
-      }
-    }
+    margin: 40px auto;
   }
 
   > button {
-    margin-top: 16px;
-    background: var(--primary-color);
-    border: none;
     height: 40px;
     width: 100%;
     border-radius: 4px;
+    border: none;
+    color: var(--secondary-color);
+    background: var(--primary-color);
+    box-shadow: var(--window-box-shadow);
+
+    &:hover {
+      background: var(--secondary-color);
+      color: var(--primary-color);
+      box-shadow: var(--box-shadow);
+    }
+  }
+
+  > input {
+    height: 40px;
+    width: 100%;
+    border-radius: 4px;
+    padding: 0 16px;
+    border: none;
   }
 `;
