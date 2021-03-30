1bafe31c87389f3d266fb50e5271f7f4f65dffce
GuyBR <gbrobbe@gmail.com>
2021-03-30 01:48:40 -0300

    S_190:Party-RefreshGoogleToken

---
+++ src/utils/refreshGoogleTokenSetup.ts
@@ -0,0 +1,15 @@
+import { GoogleLoginResponse } from 'react-google-login';
+
+export const refreshGoogleTokenSetup = (res: GoogleLoginResponse): void => {
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+
+  const refreshToken = async (): Promise<void> => {
+    const newAuthRes = await res.reloadAuthResponse();
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+    console.log('newAuthRes:', newAuthRes);
+    console.log('new auth Token', newAuthRes.id_token);
+
+    setTimeout(refreshToken, refreshTiming);
+  };
+  setTimeout(refreshToken, refreshTiming);
+};
